3bfe59029b1ecc036db08f07fefe63d19765b745
Mohammad Sujon <mohammad.sujon@outlook.com>
2021-11-08 04:01:40 +0600

    2. Logic and Conditions

---
+++ 2. Logic and Conditions/check.js
@@ -0,0 +1,7 @@
+const name = 'Mohammad Sujon';
+
+if( typeof name !== undefined ){
+    console.log('Name is defined');
+}else{
+    console.log('Name is not defined');
+}
---
+++ 2. Logic and Conditions/logicalAndOr.js
@@ -0,0 +1,8 @@
+var name = ''
+
+var fullname = name || 'Mohammad Sujon'
+console.log(fullname)
+
+var isOk = false
+
+isOk && console.log('Everything is OK')
