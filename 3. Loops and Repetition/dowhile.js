63d4c1c7dc596a805087be845e44e97168ec491d
Mohammad Sujon <mohammad.sujon@outlook.com>
2021-11-08 04:02:17 +0600

    3. Loops and Repetition

---
+++ 3. Loops and Repetition/dowhile.js
@@ -0,0 +1,26 @@
+// Do While Loop
+
+/*
+var isRunning = 0
+
+while (isRunning < 10) {
+    console.log('I am Running')
+    isRunning += 1
+}
+
+do {
+    console.log('I am Running')
+    isRunning += 1
+} while(isRunning < 10)
+
+*/
+
+var isRunning = false
+
+while (isRunning) {
+    console.log('I am Running')
+}
+
+do {
+    console.log('I am Running')
+} while(isRunning)
